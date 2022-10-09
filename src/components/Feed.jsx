186bf4511cf52e7e4205a461f38dd134b7000a28
c9long <109810526+c9long@users.noreply.github.com>
2022-10-09 09:54:31 -0400

    Revert "Eric ver"

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,15 +1,18 @@
-import { Box } from '@mui/material'
-import React from 'react'
-import Post from './Post'
+
+import { Box } from "@mui/material";
+import React from "react";
+import Posts from "./Posts";
 
 const Feed = () => {
-  return (
-    <Box flex={4} p={2}>
-      <Post/>
-      <Post/>
-      <Post/>
-    </Box>
-  )
-}
 
-export default Feed
+  return (
+    <Box flex={4} p={2} sx={{bgcolor:"background.paper"}}>
+      <Posts/>
+      <Posts/>
+      <Posts/>
+      <Posts/>
+    </Box>
+  );
+};
+
+export default Feed;
