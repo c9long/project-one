af747ff5473cc4faedf0dd3fcc279777de3b50f3
Chris <c9long@uwaterloo.ca>
2022-10-11 18:59:36 -0400

    reverting to browserrouter

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import './App.css';
 import Home from './pages/home';
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import SearchAppBar from './components/AppBar';
 import Tour from './pages/tour';
 
 function App() {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <SearchAppBar />
       <Routes>
         <Route path="/project-one/" element={<Home />} />
         <Route path="/project-one/:id" element={<Tour />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
