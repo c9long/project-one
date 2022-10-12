f4570b9c74b2fbfbb1f0e90647eef0adf7cc28bd
Chris <c9long@uwaterloo.ca>
2022-10-11 20:07:38 -0400

    changing to working hashrouter

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import './App.css';
 import Home from './pages/home';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import SearchAppBar from './components/AppBar';
 import Tour from './pages/tour';
 
 function App() {
   return (
-    <BrowserRouter>
+    <>
       <SearchAppBar />
       <Routes>
-        <Route path="/project-one/" element={<Home />} />
-        <Route path="/project-one/:id" element={<Tour />} />
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/:id" element={<Tour />} />
       </Routes>
-    </BrowserRouter>
+    </>
   );
 }
 
