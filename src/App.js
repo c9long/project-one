3cf561a81d7ad62de90cfb1d823c71855c1a2558
Chris <c9long@uwaterloo.ca>
2022-10-11 20:21:56 -0400

    making it easier to use multiple pages with router

--- src/App.js
+++ src/App.js
@@ -1,32 +1,7 @@
-import { Box, createTheme, Stack, ThemeProvider } from "@mui/material";
-import Feed from "./components/Feed";
-import Navbar from "./components/Navbar";
-import Rightbar from "./components/Rightbar";
-import Sidebar from "./components/Sidebar";
-import AddButton from "./components/AddButton";
-import { useState } from "react";
+import Home from './pages/Home';
 
 function App() {
-  const [mode, setMode] = useState("light")
-  const darkTheme = createTheme ({
-    palette: {
-      mode: mode
-    }
-  })
-
-  return (
-    <ThemeProvider theme={darkTheme}>
-      <Box bgcolor={"background.default"} color={"text.primary"}>
-        <Navbar/>
-        <Stack direction="row" spacing={2} justifyContent="space-between">
-          <Sidebar mode={mode} setMode={setMode} />
-          <Feed/>
-          <Rightbar/>
-        </Stack>
-        <AddButton/>
-      </Box>
-    </ThemeProvider>
-  );
+  return (<Home />);
 }
 
 export default App;
